e34897c5d9230c8b425b116c7f22ad3a7c7b0439
alissoncondoy <adcondoy2@utpl.edu.ec>
2023-10-26 17:45:16 -0500

    Segundo ejercicio

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -3,10 +3,11 @@ public class Ejercicio02 {
     public static void main(String[] args) {
         // Resolución del problema
         int var1 = 10;
-        int var2 = 2;
+        int var2 = 10;
         double respuesta;
-        respuesta= Math.pow(var1, 2 + var2);
+        respuesta= Math.pow(var1, 2) + var2;
         System.out.println("La respuesta es: " + respuesta);
-        //Autor ADCONDOY          
+        //Autor ADCONDOY
+        
     }   
 }
