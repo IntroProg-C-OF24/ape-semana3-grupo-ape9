a1c57e1e80f8eb66e6154bbb02fdff4c88b7a14c
nayecast <nacastillo11@utpl.edu.ec>
2023-10-26 17:07:02 -0500

    ejercicio corregido

--- Ejercicio01/src/ejercicio01/Ejercicio01.java
+++ Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -1,19 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio01;
-
-/**
- *
- * @author su-usuario-github
- */
 public class Ejercicio01 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Solución a la problemática
 
@@ -22,11 +8,11 @@ public class Ejercicio01 {
         int variable3 = 4;
         int variable4 = 3;
         int variable5 = 5;
-        int variable6 = 80;
+        int variable6 = 81;
 
         System.out.println("-------Solución a problema------------------");
         // Math.sqrt me permite obtener la raíz cuadrada de un número
-        
+        // Ejercicio corregido, la variable 6 fue cambiada
         double resultado = (variable1 + variable2 + variable3 + variable4) / 
                 variable5 + Math.sqrt(variable6);
         
