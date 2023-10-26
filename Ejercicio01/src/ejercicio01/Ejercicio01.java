7931816f60bc89a1b6e779a04c64330e4f876208
nayecast <nacastillo11@utpl.edu.ec>
2023-10-26 17:43:26 -0500

    ejercicio 3

--- Ejercicio01/src/ejercicio01/Ejercicio01.java
+++ Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -2,14 +2,12 @@ package ejercicio01;
 public class Ejercicio01 {
     public static void main(String[] args) {
         // Solución a la problemática
-
         int variable1 = 1;
         int variable2 = 2;
         int variable3 = 4;
         int variable4 = 3;
         int variable5 = 5;
         int variable6 = 81;
-
         System.out.println("-------Solución a problema------------------");
         // Math.sqrt me permite obtener la raíz cuadrada de un número
         // Ejercicio corregido, la variable 6 fue cambiada
